72c68f7c61860850f57a6a52c4ebf161fe62815d
Kaosar Ali <kaosar3662@gmail.com>
2026-01-25 23:38:46 +0600

    Added Debounce

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,20 +1,24 @@
-import React, { useCallback } from "react";
+/* eslint-disable react-hooks/immutability */
+import { useState } from "react";
 
 const Search = ({ searchTerm, onSearch }) => {
+    const [localTerm, setLocalTerm] = useState(searchTerm);
+  let timer;
 
-  const handleChange = useCallback(
-    (e) => {
-      const value = e.target.value;
-      onSearch(value);
-    },
-    [onSearch]
-  );
+  const handleDebounce = (e) => {
+    setLocalTerm(e.target.value)
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      onSearch(e.target.value);
+    }, 800);
+  };
 
   return (
     <input
       type="text"
-      value={searchTerm}
-      onChange={handleChange}
+      value={localTerm}
+      onChange={handleDebounce}
       placeholder="Search products..."
       className="border rounded px-3 py-2 w-full"
       aria-label="Search products"
